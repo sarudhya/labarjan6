18e73914ccdbaa34c26e4ce00460d9c94e66177e
sarudhya <sarudhya.achavakul@gmail.com>
2024-11-29 16:59:42 +0700

    lab6_2.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
+   int i=99, even = 0, odd = 0;
+   while(i != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> i;
+   
+   if(i != 0){
+    if(i%2 == 0){
+        even += 1;
+    }else{
+        odd += 1;
+    }
+   }
+   }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
-}
+   }
+
--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,43 @@
+#include <cmath>
 #include <iostream>
-
 using namespace std;
+
+
+double deg2rad(double a1){
+    double rad = (a1*M_PI)/180;
+    return rad;
+}
+
+double rad2deg(double a2){
+    double deg = (a2*180)/M_PI;
+    return deg;
+}
+
+double findXComponent(double l1,double l2,double a1,double a2){
+    double xl1 = l1*cos(a1);    
+    double xl2 = l2*cos(a2);
+    return xl1 + xl2; 
+}
+
+double findYComponent(double l1,double l2,double a1,double a2){
+    double yl1 = l1*sin(a1);
+    double yl2 = l2*sin(a2);
+    return yl1 + yl2; 
+}
+
+double pythagoras(double xcomp,double ycomp){
+    double x2 = pow(xcomp,2);
+    double y2 = pow(ycomp,2);
+    double ANSptg = pow((x2+y2),0.5);
+    return ANSptg;
+}
+
+void showResult(double result_vec_length,double result_vec_direction){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<endl;
+    cout << "Length of the resultant vector = " << result_vec_length<<endl;
+    cout << "Direction of the resultant vector (deg) = " << result_vec_direction<<endl;
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<endl;
+ }
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
